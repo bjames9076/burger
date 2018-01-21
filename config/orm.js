0797dc1003b881c4cebe48afdfaec0dc4cc3afcc
Brandon James <bjames9076@gmail.com>
2018-01-21 18:15:18 -0500

    started orm

---
+++ config/orm.js
@@ -0,0 +1,28 @@
+var connection = require('../config/connection.js');
+
+function printQuestionMarks(num){
+  var arr = [];
+
+  for (var i=0; i<num; i++){
+    arr.push('?')
+  };
+
+  return arr.toString();
+};
+
+function objToSql(ob){
+  
+  var arr = [];
+
+  for (var key in ob) {
+    arr.push(key + '=' + ob[key]);
+  };
+
+  return arr.toString();
+};
+
+var orm = {
+
+};
+
+module.exports=orm;
