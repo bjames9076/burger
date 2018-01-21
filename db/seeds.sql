edccf2009acad6c61438816aef65edbaf5f88d2f
Brandon James <bjames9076@gmail.com>
2018-01-21 18:02:31 -0500

    added some burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name) VALUES ('Double Cheese');
+INSERT INTO burgers(burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Veggie Burger');
