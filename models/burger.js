33351c6ec2be3fae6fc56a6b1da572a6eccc9c63
Brandon James <bjames9076@gmail.com>
2018-01-22 00:43:26 -0500

    changesed some functions

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,29 @@
 var orm = require('../config/orm.js');
 
-var burgers = {
-	all: function(cb){
-		orm.all('burgers', function(res){
-			cb(res);
-		});
-	},
-	create: function(col, vals, cb){
-		orm.create('burgers', col, vals, function(res){
-			cb(res);
-		});
-	},
-	update: function(objColVals, condition, cb){
-		orm.update('burgers', objColVals, condition, function(res){
-			cb(res);
-		});
-	}
+
+
+var burger = {
+
+  selectAll: function(callback){
+    orm.selectAll(function(res){
+      callback(res);
+    });
+  },
+
+  insertOne: function(burger_name, callback){
+    orm.insertOne(burger_name, function(res){
+      callback(res);
+    });
+  },
+
+  updateOne: function(burger_id, callback){
+    orm.updateOne(burger_id, function(res){
+      callback(res);
+    });
+  }
+
 };
 
-module.exports = burgers;
+
+
+module.exports = burger;
