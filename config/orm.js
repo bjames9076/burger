3787bd8751c83d31dd61c497b7cfcb5ee3ea36bf
Brandon James <bjames9076@gmail.com>
2018-01-22 01:02:46 -0500

    added connect

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,14 @@
 // Import Node Dependencies
 var connection = require('./connection.js');
 
-
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    };
+    console.log('connected as id ' + connection.threadId);
+  });
+  
 
 
 
