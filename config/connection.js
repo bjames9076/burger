ff158cf1fc2e75061814a2bedda95fde8d60251c
Brandon James <bjames9076@gmail.com>
2018-01-22 01:32:56 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'bootcamp',
+        host: '	cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: '	ndze41k2pt5tq3yp',
+        password: '	zu58wtieu6ar1dj9',
         database: 'burgers_db',
     });
 };
